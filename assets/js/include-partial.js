8e766da084292ea8a8096f458b45a3df37763c7d
Thyaga Sri Deshaka <tsri611@gmail.com>
2026-08-19 16:49:31 +0530

    product page iprovmens

--- assets/js/include-partial.js
+++ assets/js/include-partial.js
@@ -22,7 +22,7 @@
     var root = file.indexOf("../") === 0 ? "../" : "";
     var request = new XMLHttpRequest();
 
-    request.open("GET", file + (file.indexOf("?") === -1 ? "?v=20260819c" : "&v=20260819c"), false);
+    request.open("GET", file + (file.indexOf("?") === -1 ? "?v=20260819e" : "&v=20260819e"), false);
     request.send(null);
 
     if (request.status !== 200 && request.status !== 0) {
